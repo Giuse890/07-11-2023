47b42cde2b02ac2c2b49bc62b5a1986daa8b10b4
Giuseppe Campanelli <149474319+Giuse890@users.noreply.github.com>
2023-11-07 16:13:57 +0100

    Add files via upload

---
+++ Cliente.java
@@ -0,0 +1,36 @@
+public class Cliente {
+
+    String id;
+    String nome;
+    String email;
+    
+    public Cliente(String id, String nome, String email) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
